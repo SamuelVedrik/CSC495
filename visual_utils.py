f317813b71ab5e47112bd345144b8b9580689056
Samuel Vedrik <samuelvedrik@gmail.com>
2022-02-18 20:34:44 -0500

    Added plot encoding function

--- visual_utils.py
+++ visual_utils.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from constants import INV_NORMALIZE
 from typing import Dict
 import torch
+import plotly.graph_objects as go
 
 plt.rcParams["figure.figsize"] = (13, 7)
 
@@ -31,3 +32,20 @@ def showcase_transforms(datasets: Dict, idx=0):
     fig.tight_layout()
     plt.show()
     
+    
+def plot_encoded_values(representations, labels, dataset, title=""):
+    fig = go.Figure()
+    for id, label in dataset.id_to_labels.items():
+        points = representations[labels == id]
+        fig.add_trace(go.Scatter(
+                    x = points[:, 0],
+                    y = points[:, 1],
+                    mode="markers",
+                    name = label
+            ))
+    fig.update_layout(
+        xaxis_range = [representations[:, 0].min() - abs(representations[:, 0].min() * 0.05), representations[:, 0].max() + abs(representations[:, 0].max() * 0.05)],
+        yaxis_range = [representations[:, 1].min() - abs(representations[:, 1].min() * 0.05), representations[:, 1].max() + abs(representations[:, 1].max() * 0.05)],
+        title = title
+    )
+    fig.show()
